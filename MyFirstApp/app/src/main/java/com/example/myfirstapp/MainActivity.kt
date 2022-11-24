f44e3b942f2b84bcfe61f938b584eb7f267d48e4
Veselin Velkov <vesselin60@gmail.com>
2022-11-24 17:35:52 +0200

    First App

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnUp = findViewById<Button>(R.id.mybuttonUp)
+        val btnDown = findViewById<Button>(R.id.mybuttonDown)
+        val txtGotcha = findViewById<TextView>(R.id.textView)
+        var counter = 0
+
+        btnUp.setOnClickListener {
+            counter += 1
+            txtGotcha.text = counter.toString()
+            Toast.makeText(this, "Hey, I am a Toast!", Toast.LENGTH_LONG).show()
+        }
+        btnDown.setOnClickListener {
+            counter -= 1
+            txtGotcha.text = counter.toString()
+        }
+
     }
 }
