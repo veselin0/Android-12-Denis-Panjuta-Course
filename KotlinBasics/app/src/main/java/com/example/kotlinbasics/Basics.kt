04d03d4553281267eab1614689dba30abf926749
Veselin Velkov <vesselin60@gmail.com>
2022-11-25 09:45:07 +0200

    Datatypes Bool, Char and String

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -10,17 +10,31 @@ fun main(){
      */
 
     val myName = "Doc"
+
+    // print("Hello $myName")
     var myAge = 61
     val myByte: Byte = 14
     val myShort: Short = 125
     val myINt: Int = 12
-    val myLong: Long = 123456789
+    val myLong: Long = 12_345_67_89
 
     val myFloat: Float = 14.98F
     myFloat
     val myDouble: Double = 0.12345678977897434
     myDouble
 
+    var isSunny: Boolean = true
+    isSunny = false
+
+    val letterChar = 'A'
+    letterChar
+    val digitChar = '$'
+    digitChar
+
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
     
-    print("Hello $myName")
+    // print("First character $firstCharInStr")
+    print("Last Character $lastCharInStr")
 }
