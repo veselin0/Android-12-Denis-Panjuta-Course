7ded3808e2f52931b9fac3d9f04ee957daf2b6db
Veselin Velkov <vesselin60@gmail.com>
2022-11-25 09:29:19 +0200

    21. Datatypes of Numbers

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,8 +1,26 @@
 package com.example.kotlinbasics
 
 fun main(){
+    // immutable variable
+    // TODO: Add new functionality
+    /*
+    This
+    is a multiline
+    comment
+     */
 
     val myName = "Doc"
+    var myAge = 61
+    val myByte: Byte = 14
+    val myShort: Short = 125
+    val myINt: Int = 12
+    val myLong: Long = 123456789
+
+    val myFloat: Float = 14.98F
+    myFloat
+    val myDouble: Double = 0.12345678977897434
+    myDouble
+
     
     print("Hello $myName")
 }
