0b6c4f361625c2fdc08664dfa519753d15203b2e
Veselin Velkov <vesselin60@gmail.com>
2022-11-26 08:51:51 +0200

    42 Exercise Loops

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -198,6 +198,33 @@ fun main(){
         print("  $j  ")
     }
 
+    /*
+    Exercise Loops
+Write a for loop that runs from 0 to 10000
+Once it's at 9001 it should write "IT'S OVER 9000!!!"
+
+Write a while loop that checks the humidity (not the humidityLevel).
+The variable humidityLevel starts at 80. The variable humidity is initialized with "humid".
+If it is "humid" then it should reduce the "humidityLevel" by 5 and print "humidity decreased"
+Once the humidityLevel is below 60 it should print "it's comfy now" and set the humidity to "comfy"
+     */
+    for (i in 0..10000) {
+        if (i == 9001) {
+            println("IT'S OVER 9000!!!")
+        }
+    }
+    println("for loop done")
+
+    var humidity = "humid"
+    var humidityLevel = 80
+    while (humidity == "humid") {
+        humidityLevel -= 5
+        println("Humidity decreased")
+        if (humidityLevel < 60) {
+            humidity = "comfy"
+            println("It's comfy now")
+        }
+    }
 }
 
 
