857bbdaba240891bb09b397d40166c6415f86cc8
Veselin Velkov <vesselin60@gmail.com>
2022-11-26 17:58:28 +0200

    55. Exercise Classes and Objects

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/helloworld.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/helloworld.kt
@@ -1,17 +1,26 @@
 package com.example.kotlinbasics
 
-// 54. Classes And Initializers
+// 55. Exercise Classes and Objects
+/*
+Please create a class called MobilePhone. It should have three primary constructor variables, osName, brand and model.
+
+Use the initializer to print those details.
+
+Create three objects of that class in the Main function.
+
+Examples of phones would be:
+Samsung Galaxy S20 Ultra. Here the osName is Android, brand is Samsung, model is Galaxy S20 Ultra
+ */
 fun main () {
-    // Creating instances of clases
-    var gocho = Person("Gocho", "Kochev")
-    var john = Person()
-    var dochev = Person(lastName = "Dochev")
+    var samsung = MobilePhone("Android 12", "Samsung", "Galaxy S20 Ultra")
+    var umi = MobilePhone("Android 5.0", "Umi", "Umi Iron")
+    var xiaomi = MobilePhone("Android 15", "Xiaomi", "Sup22")
 
 }
 
-class Person(firstName: String = "John", lastName: String = "Doe") {
+class MobilePhone (osName: String, brand: String, model: String) {
     init {
-        println("Created a new Person object with a firstName $firstName " +
-                "and lastName $lastName")
+        println("Created a new MobilePhone object with a osName $osName, " +
+                "brand $brand and model $model")
     }
 }
