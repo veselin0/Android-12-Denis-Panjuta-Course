b50f62168e6a2ece977b02e58854fad818d8f70f
Veselin Velkov <vesselin60@gmail.com>
2022-11-30 12:42:04 +0200

    Adding a DatePickerDialog and OnDateSetListener

--- YOURAGEINMINUTESCALCULATOR/app/src/main/java/com/example/yourageinminutescalculator/MainActivity.kt
+++ YOURAGEINMINUTESCALCULATOR/app/src/main/java/com/example/yourageinminutescalculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.yourageinminutescalculator
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
+                Toast.makeText(this, "Datapicker works", Toast.LENGTH_LONG).show()
+            },
+            year,
+            month,
+            day
+            ).show()
+
+        Toast.makeText(this,
+            "btnDataPicker pressed", Toast.LENGTH_LONG).show()
     }
 }
