3848fe452ccfd9f34a353bb4fe812047112f4543
Veselin Velkov <vesselin60@gmail.com>
2022-12-02 15:55:42 +0200

    94. Building The Calculator UI using Linear Layouts part 1

---
+++ DenisCalculator/app/src/main/java/com/example/deniscalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.deniscalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+    }
+}
