8ddd730a4bbc6ca1b996f90482b2938b8e77b495
Veselin Velkov <vesselin60@gmail.com>
2022-12-06 11:07:44 +0200

    107. Setting up The questions UI and Connecting the UI to the Model Part 2

--- MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/QuizQuestionsActivity.kt
@@ -1,15 +1,54 @@
 package com.example.myquizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progress_bar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
+
+        for (i in questionsList) {
+            Log.e("Questions", i.question)
+        }
+
+        var currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+        ivImage?.setImageResource(question.image)
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
 }
